0c00c91dfa035956bf5783e554b1756bc2edb181
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-04-10 03:02:46 +0300

    Mishanya

--- front/FoodFeatures/Extensions/GeneralConstants.swift
+++ front/FoodFeatures/Extensions/GeneralConstants.swift
@@ -10,5 +10,5 @@ enum GeneralConstants {
     static let viewControllerBackgroundColor: UIColor = UIColor(red: 192/255, green: 114/255, blue: 114/255, alpha: 1)
     static let buttonsBackgroundColor: UIColor = UIColor(red: 199/255, green: 60/255, blue: 63/255, alpha: 1)
     static let translatesAutoresizingMaskIntoConstraints: Bool = false
-    static let baseURL = "http://10.193.90.85:8000"
+    static let baseURL = "http://10.193.95.190:8000"
 }
